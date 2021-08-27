69cabca5475a893e68b118ec5cd2c9495f7fb706
amir <amhheydari11@gmail.com>
2021-08-27 12:25:35 -0700

    4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { SidenavService } from './services/sidenav.service';
+import { onMainContentChange } from './animations/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [ onMainContentChange ]
 })
 export class AppComponent {
   title = 'azmoon';
+  public onSideNavChange: boolean | undefined;
+
+  constructor(private _sidenavService: SidenavService) {
+    this._sidenavService.sideNavState$.subscribe( res => {
+      console.log(res)
+      this.onSideNavChange = res;
+    })
+  }
 }
