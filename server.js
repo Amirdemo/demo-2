2c344a6e2221f97288fb9a7c36bb23b5cef997a0
amir <amhheydari11@gmail.com>
2021-10-03 22:40:20 +0330

    2

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/azmoon'));
+app.use(express.static(__dirname + '/dist/azmoon'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/azmoon/' }),
-);
+app.get('/*', (req, res, next) => {
+    res.sendFile(path.join(__dirname + '/dist/dictionary/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8000);
